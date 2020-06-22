b92b62324fbe4fe9c872f5a12af895220808ecae
MENOUER1 <menouerr2@gmail.com>
2020-06-22 12:13:20 +0200

    add files of theme

---
+++ front-page.php
@@ -0,0 +1,132 @@
+<?php get_header(); ?>
+
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/caper.jpg') ?>);"></div>
+    <div class="page-banner__content container t-center c-white">
+      <h1 class="headline headline--large">Bienvenue!</h1>
+      <h2 class="headline headline--medium">Dans notre association.</h2>
+      <h3 class="headline headline--small">Pour les préventions et victimes de l'amiante. </h3>
+      <a href="#" class="btn btn--large btn--blue">Visiter notre site</a>
+    </div>
+  </div>
+
+  <div class="full-width-split group">
+    <div class="full-width-split__one">
+      <div class="full-width-split__inner">
+        <h2 class="headline headline--small-plus t-center">Evenements</h2>
+         
+      <?php
+
+      $homepageEvents = new WP_Query(array(
+        'posts_per_page' => 2,
+        'post_type' => 'event'
+      ));
+
+      while($homepageEvents->have_posts()) {
+        $homepageEvents->the_post(); ?>
+        <div class="event-summary">
+          <a class="event-summary__date t-center" href="#">
+            <span class="event-summary__month"><?php 
+            $eventDate = new DateTime(the_field('event_date'));
+           echo $eventDate->format('M')
+             ?></span>
+            <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>  
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+            <p><?php if (has_excerpt()) {
+              echo get_the_excerpt();
+            } else {
+              echo wp_trim_words(get_the_content(), 18);
+            }
+              ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+          </div>
+        </div>
+      <?php }
+      ?>
+       
+        
+        
+        
+        <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">Tout les evenements</a></p>
+
+      </div>
+    </div>
+    <div class="full-width-split__two">
+      <div class="full-width-split__inner">
+        <h2 class="headline headline--small-plus t-center">Notre Blog</h2>
+       <?php
+       $homepagePosts = new WP_Query(array(
+         'posts_per_pages' => 2
+       ));
+       while ($homepagePosts->have_posts()){
+         $homepagePosts->the_post(); ?>
+         <div class="event-summary">
+          <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+            <span class="event-summary__month">Jan</span>
+            <span class="event-summary__day">20</span>  
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
+            <p><?php if (has_excerpt()) {
+              echo get_the_excerpt();
+            } else {
+              echo wp_trim_words(get_the_content(), 18);
+            }
+              ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+          </div>
+        </div>
+        <div class="event-summary">
+          <a class="event-summary__date event-summary__date--beige t-center" href="#">
+            <span class="event-summary__month"><?php the_time('M'); ?></span>
+            <span class="event-summary__day"><?php the_time('d'); ?></span>  
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+            <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
+          </div>
+        </div>
+         
+       <?php } wp_reset_postdata();
+       ?>
+        
+        
+        <p class="t-center no-margin"><a href="<?php echo site_url('/blog');?>" class="btn btn--yellow">View All Blog Posts</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="hero-slider">
+  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/caper5.jpg') ?>);">
+    <div class="hero-slider__interior container">
+      <div class="hero-slider__overlay">
+        <h2 class="headline headline--medium t-center">Free Transportation</h2>
+        <p class="t-center">All students have free unlimited bus fare.</p>
+        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/caper6.jpg') ?>);">
+    <div class="hero-slider__interior container">
+      <div class="hero-slider__overlay">
+        <h2 class="headline headline--medium t-center">An Apple a Day</h2>
+        <p class="t-center">Our dentistry program recommends eating apples.</p>
+        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/caper7.jpg') ?>);">
+    <div class="hero-slider__interior container">
+      <div class="hero-slider__overlay">
+        <h2 class="headline headline--medium t-center">Free Food</h2>
+        <p class="t-center">Fictional University offers lunch plans for those in need.</p>
+        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+  
+ <?php get_footer();
+  ?>
